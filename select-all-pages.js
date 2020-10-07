095deeda3931af8b34b290b302069c7e2a8dffcb
Chih-Yu Hsiao (Jay) <1033992+jcyhsiao@users.noreply.github.com>
2020-10-07 16:05:51 -0400

    Update select-all-pages.js
    Refactored to 1) use querySelector(All), and 2) "click" delete button at the end (still requires manual confirmation).

--- select-all-pages.js
+++ select-all-pages.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Select all pages
 // @namespace    https://github.com/jcyhsiao/canvas-usersripts
-// @version      2020.10.07-1
+// @version      2020.10.07-2
 // @description  One button to select all pages on the Pages index page
 // @author       Chih-Yu (Jay) Hsiao
 // @include      https://*.*instructure.com/courses/*/pages
@@ -9,23 +9,23 @@
 // @grant        none
 // ==/UserScript==
 
-// In Chrome-based browsers, access via the right click context menu. 
-// TODO: Execute when page finishes loading; one challenge is how to account for additional pages that are only loaded upon infinite scrolling. 
+// In Chrome-based browsers, access via the right click context menu.
+// TODO: Add a button. One challenge is how to account for additional pages that are only loaded upon infinite scrolling.
+
 
 (function() {
     'use strict';
-    
-    // Select all checkboxes. For each checkbox, if it is not disabled (i.e. front page), set its checked property to true. 
-    // Then, set the delete button's disabled property to false.
-    
-    const checkboxes = document.getElementsByClassName('select-page-checkbox');
+
+    // Select all checkboxes. For each checkbox, fire its click event if it's not already checked.
+    const checkboxes = document.querySelectorAll('input[type=checkbox].select-page-checkbox');
     for (const checkbox of checkboxes) {
-        if (checkbox.disabled === false) {
-            checkbox.checked = true;
+        if (checkbox.checked === false) {
+            checkbox.click();
         }
     }
-    const delete_button = document.getElementsByClassName('delete_pages');
-    if (delete_button.length === 1) {
-        delete_button[0].disabled = false;
-    }
+
+    // Then, "click" the delete button.
+    const delete_button = document.querySelector('button.delete_pages');
+    delete_button.click();
+
 })();
