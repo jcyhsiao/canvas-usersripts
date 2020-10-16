1e408e14194a6c210dff716ca68e0dab9e4990ac
Chih-Yu Hsiao (Jay) <1033992+jcyhsiao@users.noreply.github.com>
2020-10-16 16:00:33 -0400

    Update delete-all-discussions.js
    Added some pieces just in case we can one day do this for the quizzes page. 
    Identifying the new button correctly.

--- delete-all-discussions.js
+++ delete-all-discussions.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Delete all discussions
+// @name         Delete all discussions on the Discussions index page
 // @namespace    https://github.com/jcyhsiao/canvas-usersripts
 // @version      2020.10.09-2
-// @description  One button to delete all discussions on the Discussions index page
+// @description  One button to delete all items on the Discussions index page
 // @author       Chih-Yu (Jay) Hsiao
 // @include      https://*.*instructure.com/courses/*/discussion_topics
 // @run-at       document-idle
@@ -11,23 +11,34 @@
 
 // Triggered by clicking button
 function execute() {
+    let is_discussions = false;
+
     // Gather all discussion delete buttons
-    const delete_buttons = document.querySelectorAll('.ic-discussion-content-container .discussions-index-manage-menu button');
+    // Check if we're on either Assignments or Quizzes
+    // NOTE: Just in case - currently it's not possible to automate this for the quizzes page
+    let manage_menu_buttons = document.querySelectorAll('div.ig-admin i.icon-more');
+    if (manage_menu_buttons.length === 0) {
+        manage_menu_buttons = document.querySelectorAll('span.ic-discussion-content-container span.discussions-index-manage-menu button');
+        is_discussions = true;
+    }
 
     // User confirmation
-    if (window.confirm("Do you really want to delete all discussion boards?")) {
-        for (let i = 0; i < delete_buttons.length; i++) {
-            console.log(`deleting button ${i + 1} of ${delete_buttons.length}`);
-            let button = delete_buttons[i];
-            // Click delete button to reveal management menu
-            button.click();
-            let menu_option_delete = document.querySelector('#delete-discussion-menu-option');
-            // Click delete button in management menu
-            menu_option_delete.click();
-            // Which brings up a confirmation dialog
-            let confirm_delete = document.querySelector('#confirm_delete_discussions');
-            // Click delete confirmation button to delete
-            confirm_delete.click();
+    if (window.confirm("Do you really want to delete all items?")) {
+        for (const button of manage_menu_buttons) {
+            if (is_discussions) {
+                button.click();
+                let menu_option_delete = document.querySelector('#delete-discussion-menu-option');
+                // Click delete button in management menu
+                menu_option_delete.click();
+                // Which brings up a confirmation dialog
+                let confirm_delete = document.querySelector('#confirm_delete_discussions');
+                // Click delete confirmation button to delete
+                confirm_delete.click();
+            }
+            else {
+                // This is not implemented
+                return;
+            }
         }
     }
 
@@ -40,15 +51,15 @@ function execute() {
     // Adding the new button as the first item in #content
     const div_content = document.querySelector('#content-wrapper');
 
-    const button_select_all = document.createElement('button');
-    button_select_all.onclick = execute;
-    button_select_all.innerHTML = 'Delete All Discussions';
-    button_select_all.classList.add('btn');
-    button_select_all.setAttribute('type', 'button');
-    button_select_all.setAttribute('tabIndex', '0');
-    button_select_all.setAttribute('id', 'delete_all_btn');
+    const new_button = document.createElement('button');
+    new_button.onclick = execute;
+    new_button.innerHTML = 'Delete All Items';
+    new_button.classList.add('btn');
+    new_button.setAttribute('type', 'button');
+    new_button.setAttribute('tabIndex', '0');
+    new_button.setAttribute('id', 'delete_all_btn');
     // button_select_all.style.float = 'right';
-    button_select_all.style.margin = '1em';
+    new_button.style.margin = '.5em';
 
-    div_content.insertBefore(button_select_all, div_content.childNodes[1]);
+    div_content.insertBefore(new_button, div_content.childNodes[1]);
 })();
